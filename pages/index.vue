27f8f75fdd35ba70e33f30e67bfedf4795d81ef5
Min-thike-kyaw <61089445+Min-thike-kyaw@users.noreply.github.com>
2023-01-20 12:37:44 +0630

    Admin Dashboard

--- pages/index.vue
+++ pages/index.vue
@@ -8,17 +8,14 @@
     </NuxtLink> -->
     <Button label="Secondary" class="p-button-secondary" />
     <p class="text-lg">Localhost</p>
-    <Sidebar :visible.sync="visibleLeft">
-    Content
-  </Sidebar>
+
 
 
   </div>
 </template>
 
 <script>
-// import Button from 'primevue/button';
-// import Sidebar from 'primevue/sidebar';
+
 import NavBar from '~/components/layouts/NavBar.vue';
 export default {
   name: 'IndexPage',
